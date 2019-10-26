33fa0cab0c296e2b70e91279b54a85ee2e49c0a0
Pancho Labrador <plabradorjr@gmail.com>
2019-10-26 03:02:46 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,11 +3,11 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAddressParser(emails)
+class EmailAddressParser
 
 attr_accessor :emails
 
-  def initialized
+  def initialized(emails)
     @emails = emails.split(", ")
   end
 
